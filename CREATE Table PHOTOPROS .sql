b82c3af0a46a254f793f00a206ea40b2e59933d1
boubakermohamedali <72795659+boubakermohamedali@users.noreply.github.com>
2023-06-30 14:47:47 +0200

    Create CREATE Table PHOTOPROS .sql

---
+++ CREATE Table PHOTOPROS .sql
@@ -0,0 +1,8 @@
+CREATE Table PHOTOPROS (
+  photopro_id int PRIMARY KEY  not null,
+  photopro_address varchar (512) not null,
+  photopro_president_name varchar (256) not null,
+  photopro_email varchar (320) not null,
+  RCS varchar (256) not null
+);
+
