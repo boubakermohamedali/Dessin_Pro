be30ecdbb54e10d57a231c6ea7c91d4942cd0c53
boubakermohamedali <72795659+boubakermohamedali@users.noreply.github.com>
2023-07-04 16:01:26 +0200

    First commit

---
+++ CREATE TABLE CATEGORY.sql
@@ -0,0 +1,6 @@
+CREATE TABLE CATEGORY(
+    category_id INT PRIMARY KEY NOT NULL,
+    category_name VARCHAR(45) NOT NULL,
+    category_description VARCHAR(256) NOT NULL,
+    category_update DATE NOT NULL
+);
